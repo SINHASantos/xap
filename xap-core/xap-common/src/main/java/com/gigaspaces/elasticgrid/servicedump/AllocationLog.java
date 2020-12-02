9857fd73bf78214af95c2d86ad9865509668a717
mishel liberman <mishel170@gmail.com>
2020-12-02 09:04:09 +0200

    add date to allocation log folder

--- xap-core/xap-common/src/main/java/com/gigaspaces/elasticgrid/servicedump/AllocationLog.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/elasticgrid/servicedump/AllocationLog.java
@@ -1,14 +1,22 @@
 package com.gigaspaces.elasticgrid.servicedump;
 
+import java.util.Date;
+
 public class AllocationLog {
     private final String allocId;
     private final String stdout;
     private final String stderr;
+    private final Date timestamp;
 
-    public AllocationLog(String allocId, String stdout, String stderr) {
+    public AllocationLog(String allocId, String stdout, String stderr, Date timestamp) {
         this.allocId = allocId;
         this.stdout = stdout;
         this.stderr = stderr;
+        this.timestamp = timestamp;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
     }
 
     public String getAllocId() {
